0d2fba29213b205e0662004c1b570069e96fea5e
Beato <121357043+urc4@users.noreply.github.com>
2023-01-09 12:28:54 -0300

    Create dealer class
    Forgot to add it the first time around, we will see if it is
    necessary. Otherwise we can simply use the player class we
    already have.

--- main.py
+++ main.py
@@ -45,6 +45,18 @@ class Player:
    def __str__(self):
       return f'Player {self.name} has {len(self.all_cards)} cards.'
 
-   
+class Dealer:
+   def __init__(self):
+      self.name = 'Dealer'
+      self.all_cards = []
+
+   def add_cards(self,new_cards):
+      if type(new_cards) == type([]):
+         self.all_cards.extend(new_cards)
+      else:
+         self.all_cards.append(new_cards)
+
+   def __str__(self):
+      return f'Player {self.name} has {len(self.all_cards)} cards.'
 
 
